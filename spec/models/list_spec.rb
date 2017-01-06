4401ef95688b990fbc9650a5dd51ba30a321adae
Peter Jang <peter.x.jang@gmail.com>
2017-01-05 19:25:22 -0600

    Finish all list method tests

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -12,4 +12,47 @@ RSpec.describe List, type: :model do
       end
     end
   end
+
+  describe '#snooze_all_tasks!' do
+    it 'should snooze each task by 1 hour' do
+      list = List.create(name: "Chores")
+      task_deadline = Time.now
+      task1 = Task.create(deadline: task_deadline, list_id: list.id)
+      task2 = Task.create(deadline: task_deadline, list_id: list.id)
+      task3 = Task.create(deadline: task_deadline, list_id: list.id)
+      list.snooze_all_tasks!
+      list.tasks.each do |task|
+        expect(task.deadline).to eq(task_deadline + 1.hour)
+      end
+    end
+  end
+
+  describe '#total_duration' do
+    it 'should return the sum of all the task durations' do
+      list = List.create(name: "Chores")
+      task1 = Task.create(duration: 50, list_id: list.id)
+      task2 = Task.create(duration: 150, list_id: list.id)
+      expect(list.total_duration).to eq(200)
+    end
+  end
+
+  describe '#incomplete_tasks' do
+    it 'should return an array of incomplete tasks' do
+      list = List.create(name: "Chores")
+      task1 = Task.create(complete: true, list_id: list.id)
+      task2 = Task.create(complete: false, list_id: list.id)
+      task3 = Task.create(complete: false, list_id: list.id)
+      expect(list.incomplete_tasks.length).to eq(2)
+    end
+  end
+
+  describe '#favorite_tasks' do
+    it 'should return an array of incomplete tasks' do
+      list = List.create(name: "Chores")
+      task1 = Task.create(favorite: true, list_id: list.id)
+      task2 = Task.create(favorite: false, list_id: list.id)
+      task3 = Task.create(favorite: false, list_id: list.id)
+      expect(list.favorite_tasks.length).to eq(1)
+    end
+  end
 end
