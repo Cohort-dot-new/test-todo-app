ebb560de73f1ffd887e98ce332c4a007aac31c2f
Peter Jang <peter.x.jang@gmail.com>
2017-01-05 18:18:36 -0600

    Use create instead of new/save

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,12 +3,9 @@ require 'rails_helper'
 RSpec.describe List, type: :model do
   describe '#complete_all_tasks!' do
     it 'should mark all tasks from the list as complete' do
-      list = List.new(name: "Chores")
-      list.save
-      task1 = Task.new(complete: false, list_id: list.id)
-      task1.save
-      task2 = Task.new(complete: false, list_id: list.id)
-      task2.save
+      list = List.create(name: "Chores")
+      task1 = Task.create(complete: false, list_id: list.id)
+      task2 = Task.create(complete: false, list_id: list.id)
       list.complete_all_tasks!
       list.tasks.each do |task|
         expect(task.complete).to eq(true)
